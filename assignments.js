ce9f63097b8aa77b90e1613f8da49ca0327b9f22
hemant sikarwar <94857718+hemantsikarwar2212@users.noreply.github.com>
2025-02-21 15:28:25 +0000

    commit

--- assignments.js
+++ assignments.js
@@ -25,4 +25,26 @@ function isPalindrome(str) {
     let hours = date.getHours(); 
     console.log(`time is : ${hours}:${min}:${seconds} `);
   } 
-  setInterval( clock,1000)
+
+  function clock2(){
+    for (let i = 0; i < 60; i++) {
+      let seconds = i;
+      let min = i;
+      let hrs = i;
+      console.log(`${hrs}:${min}:${seconds}`);
+      
+    }
+  }
+
+  function getTime24() {
+    const now = new Date();
+    const hours = String(now.getHours()).padStart(2, '0');
+    const minutes = String(now.getMinutes()).padStart(2, '0');
+    const seconds = String(now.getSeconds()).padStart(2, '0');
+    console.log( `${hours}:${minutes}::${seconds}`);
+}
+
+//console.log(getTime24());
+
+
+  setInterval( getTime24,1000)
---
+++ callbacksAndPromises.js
@@ -0,0 +1,15 @@
+/*
+    Write a function that returns a promise that resolves after n seconds have passed, where n is passed as an argument to the function.
+*/
+
+function check(){
+    console.log("my promise is fullfilled");
+    
+}
+
+
+let mypromise = new Promise((resolve,reject)=>{
+setTimeout(,1000)
+
+})
+console.log(mypromise);
