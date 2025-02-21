86ce1f2350a1415932ca7ff030bc28ddb1062166
hemant sikarwar <94857718+hemantsikarwar2212@users.noreply.github.com>
2025-02-21 16:52:36 +0000

    config commit

--- callbacksAndPromises.js
+++ callbacksAndPromises.js
@@ -1,15 +1,20 @@
 /*
     Write a function that returns a promise that resolves after n seconds have passed, where n is passed as an argument to the function.
 */
-
-function check(){
-    console.log("my promise is fullfilled");
-    
+function square(n){
+return n*n;
 }
+function cube(n){
+return n*n*n;
+}
+function quad(n){
+return n*n*n*n;
+}
+ function sumOfSomething(a,b,cb1) {
+    let num1 = cb1(a);
+    let num2 = cb1(b)
+    return num1+num2;
+}
+let result = sumOfSomething(2,2,square);
+console.log(result);
 
-
-let mypromise = new Promise((resolve,reject)=>{
-setTimeout(,1000)
-
-})
-console.log(mypromise);
