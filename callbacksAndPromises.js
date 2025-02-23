74597d2271dcab6e2e993fb213b53ca5a9b596ea
hemant sikarwar <94857718+hemantsikarwar2212@users.noreply.github.com>
2025-02-23 11:37:06 +0000

    ok

--- callbacksAndPromises.js
+++ callbacksAndPromises.js
@@ -18,8 +18,9 @@ return n*n*n*n;
 let result = sumOfSomething(2,2,square);
 console.log(result);
 
+const { error } = require('console');
 ///////async functions//////////
-const fs= require('fs');
+//const fs= require('fs');
 
 function onDone(){
 console.log("done functioning");
@@ -27,8 +28,41 @@ console.log("done functioning");
 function onDone2(){
 console.log("done functioning2");
 }
-console.log("hi there");
-setTimeout(onDone2,1000);
-fs.readFile('a.txt',onDone);
-console.log("after sometime");
+// console.log("hi there");
+// setTimeout(onDone2,1000);
+// fs.readFile('a.txt',onDone);
+// console.log("after sometime");
+
+/*
+    Write a function that returns a promise that resolves after n seconds have passed, where n is passed as an argument to the function.
+    */
+
+    function wait(val) {
+        setTimeout(()=>{
+            console.log("after some time" ,val);
+            
+        },1000)
+    }
+    const fs = require('fs')
+let p = new Promise((resolve, reject) => {
+   fs.readFile('a.txt','utf-8',(err,data)=>{
+        setTimeout(()=>{
+resolve(data)
+        },1000)
+        
+
+    if(err) reject(err);
+    
+   })
+
+})
+p.then((val)=>{
+    console.log(`now it is good to go ${val}`);
+    
+})
+
+p.catch((err)=>{
+    console.log("error");
+    
+})
 
